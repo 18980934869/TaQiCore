3f96ea0ce9b49e9e4da9490295b4b308bf261086
TaQi <TQ@taqi-mac-mini.local>
2016-06-20 14:18:46 +0800

    init

---
+++ TaQiCore/TaQiCore.h
@@ -0,0 +1,19 @@
+//
+//  TaQiCore.h
+//  TaQiCore
+//
+//  Created by TaQi on 16/6/20.
+//  Copyright © 2016年 TaQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TaQiCore.
+FOUNDATION_EXPORT double TaQiCoreVersionNumber;
+
+//! Project version string for TaQiCore.
+FOUNDATION_EXPORT const unsigned char TaQiCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaQiCore/PublicHeader.h>
+
+
